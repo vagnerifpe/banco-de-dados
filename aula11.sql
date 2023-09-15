6ab8177e50e0f2d6f8688039cc722d1a89918fab
vagnerifpe <144134888+vagnerifpe@users.noreply.github.com>
2023-09-14 21:09:07 -0300

    Update aula11.sql

--- aula11.sql
+++ aula11.sql
@@ -1,11 +1,12 @@
 CREATE TABLE aluno (
     id integer PRIMARY KEY AUTOINCREMENT,
+    nome varchar(255),
     nota1 FLOAT,
     nota2 FLOAT
 );
 
-INSERT INTO aluno (nota1, nota2) VALUES (8.5, 9.0);
-INSERT INTO aluno (nota1, nota2) VALUES (7.0, 6.5);
-INSERT INTO aluno (nota1, nota2) VALUES (9.0, 8.0);
+INSERT INTO aluno (nome,nota1, nota2) VALUES ("aluno 1",8.5, 9.0);
+INSERT INTO aluno (nome,nota1, nota2) VALUES ("aluno 2",7.0, 6.5);
+INSERT INTO aluno (nome, nota1, nota2) VALUES ("aluno 3", 9.0, 8.0);
 
 SELECT id, AVG(nota1 + nota2) as media FROM aluno WHERE id = 1;
